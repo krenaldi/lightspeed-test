31a997b66d9c5df672efe00cd449cf2bdd93948e
Kris Renaldi <krenaldi@comcast.net>
2020-07-03 16:41:07 -0400

    Set up mySQL schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS lightspeed_db;
+
+USE lightspeed_db;
+
+-- Create the table for Employees assigned to Projects
+CREATE TABLE member
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- primary key column
+    first_name VARCHAR(50) NOT NULL
+);
+
+-- Create the table for Projects
+CREATE TABLE project
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- primary key column
+    title VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- Create the table for Tasks on Each Project
+CREATE TABLE tasks
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- primary key column
+    task_name VARCHAR(50) NOT NULL,
+    duration INT NOT NULL,
+    project_id INT UNSIGNED NOT NULL,
+    INDEX proj_idx (project_id),
+    CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE,
+    member_id INT UNSIGNED NOT NULL,
+    INDEX member_idx (member_id),
+    CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES member(id) ON DELETE CASCADE
+);
---
+++ db/seed.sql
@@ -0,0 +1,31 @@
+USE lightspeed_db;
+
+INSERT INTO member (first_name)
+VALUES ('Stuart'),
+('Tyler'),
+('Adam'),
+('Lan');
+
+-- Insert rows into table 'Project'
+INSERT INTO project
+( -- columns to insert data into
+ title
+)
+VALUES ('E-Commerce Website'), 
+('Websocket Updates'), 
+('Angular Upgrade');
+
+
+-- Insert rows into table 'Tasks'
+INSERT INTO tasks
+(task_name, duration, project_id, member_id)
+VALUES
+('Product Pages', 5, 1, 3),
+( 'Shopping Cart', 10, 1, 2),
+( 'My Account', 5, 1, 3),
+( 'Add to Socket.IO', 2, 2, 1),
+( 'Enable Broadcasting', 5, 2, 1),
+( 'Adjust Interface', 3, 2, 1),
+( 'Upgrade Angular', 15, 1, 4),
+( 'Fix Broken Things', 10, 1, 1),
+( 'Test', 10, 1, 4);
