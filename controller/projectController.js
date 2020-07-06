4077b9bea33a9b020b2c1451b7130fe56a822705
Kris Renaldi <krenaldi@comcast.net>
2020-07-06 00:29:51 -0400

    Created controller routes for retrieving all data

---
+++ controller/projectController.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the model (project.js) to use its database functions.
+const project = require("../models/project.js");
+
+// GET route for ALL Employees on index.hbs
+router.get("/", function (req, res) {
+    project.getEmployees(function (result) {
+        res.render("employees", { member_data: result });
+    });
+});
+
+// GET route for ALL Projects by Employee
+router.get("/projects/:id", function (req, res) {
+    let condition = req.params.id;
+    project.getProjects(condition, function (result) {
+        res.render("projects", { project_data: result });
+    });
+});
+
+// GET route for ALL Tasks by Projects 
+router.get("/tasks/:id", function (req, res) {
+    let condition = req.params.id;
+    project.getTasks(condition, function (result) {
+        res.render("projects", { project_data: result });
+    });
+});
+
+module.exports = router;
