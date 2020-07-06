f6f5fcf64ccda696c89669c7ad26483316b614bf
Kris Renaldi <krenaldi@comcast.net>
2020-07-06 00:32:38 -0400

    Modified parameter for getProjects & getTasks to reflect different foreign keys in tableOne

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ const connection = require("./connection.js");
 const { query } = require("express");
 
 const orm = {
-    getEmployees: function(table, cb) {
+    getEmployees: function (table, cb) {
         const queryString = "SELECT * FROM ??";
         connection.query(queryString, [table], function (err, result) {
             if (err) {
@@ -12,26 +12,29 @@ const orm = {
             cb(result);
         });
     },
-    getProjects: function (tableOne, tableTwo, tableThree, tableOneCol, tableTwoCol, tableThreeCol, tableOneFk, condition, cb) {
+    getProjects: function (tableOne, tableTwo, tableThree, tableOneCol, tableTwoCol, tableThreeCol, tableOneFkOne, tableOneFkTwo, condition, cb) {
         const queryString = "SELECT ??.??, ??.?? AS 'Project Title', SUM(??.??) AS 'Estimated Hours' FROM ?? LEFT JOIN (??, ??) ON (??.??=??.id AND ??.??=??.id) WHERE ??.id = ? GROUP BY ??.??";
         console.log(queryString);
 
-        connection.query(queryString, [tableTwo, tableTwoCol, tableThree, tableThreeCol, tableOne, tableOneCol, tableOne, tableTwo, tableThree, tableOne, tableOneFk, tableTwo, tableOne, tableOneFk, tableThree, tableTwo, condition, tableThree, tableThreeCol], function (err, result) {
+        connection.query(queryString, [tableTwo, tableTwoCol, tableThree, tableThreeCol, tableOne, tableOneCol, tableOne, tableTwo, tableThree, tableOne, tableOneFkOne, tableTwo, tableOne, tableOneFkTwo, tableThree, tableTwo, condition, tableThree, tableThreeCol], function (err, result) {
             if (err) {
                 throw err;
             }
             cb(result);
         });
     },
-    getTasks: function (tableOne, tableTwo, tableThree, tableOneCol, tableTwoCol, tableThreeCol, tableOneFk, condition, cb) {
+    getTasks: function (tableOne, tableTwo, tableThree, tableOneCol, tableTwoCol, tableThreeCol, tableOneFkOne, tableOneFkTwo, condition, cb) {
         const queryString = "SELECT ??.?? AS 'Project Title', ??.?? AS 'Tasks', ??.?? AS 'Assigned To', ??.?? AS 'Estimated Hours' FROM ?? LEFT JOIN (??, ??) ON (??.??=??.id AND ??.??=??.id) WHERE ??.id = ?";
         console.log(queryString);
 
-        connection.query(queryString, [tableThree, tableThreeCol, tableOne, tableOneCol, tableTwo, tableTwoCol, tableThree, tableThreeCol, tableOne, tableOneCol, tableOne, tableTwo, tableThree, tableOne, tableOneFk, tableTwo, tableOne, tableOneFk, tableThree, tableTwo, condition], function (err, result) {
+        connection.query(queryString, [tableThree, tableThreeCol, tableOne, tableOneCol, tableTwo, tableTwoCol, tableThree, tableThreeCol, tableOne, tableOneCol, tableOne, tableTwo, tableThree, tableOne, tableOneFkOne, tableTwo, tableOne, tableOneFkTwo, tableThree, tableTwo, condition], function (err, result) {
             if (err) {
                 throw err;
             }
             cb(result);
         });
     }
-}
+};
+
+// Export the orm object for the model (project.js).
+module.exports = orm;
