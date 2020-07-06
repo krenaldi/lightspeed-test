58a4c21c785a285295b390016c176d2caf482cef
Kris Renaldi <krenaldi@comcast.net>
2020-07-06 00:30:18 -0400

    Created project.js model

---
+++ model/project.js
@@ -0,0 +1,22 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+const project = {
+    getEmployees: function (cb) {
+        orm.getEmployees("employees", function (res) {
+            cb(res)
+        });
+    },
+    getProjects: function (condition, cb) {
+        orm.getProjects("tasks", "employees", "projects", "duration", "first_name", "title", "employee_id", "project_id", condition, function (res) {
+            cb(res);
+        });
+    },
+    getTasks: function (condition, cb) {
+        orm.getTasks("tasks", "employees", "projects", "duration", "first_name", "title", "employee_id", "project_id", condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = project;
