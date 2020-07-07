666763629f5c8b47a8abf350dd5c83cd60632ab9
Kris Renaldi <krenaldi@comcast.net>
2020-07-07 16:08:45 -0400

    Changed declaration of connection in connection.js to let

--- db/schema.sql
+++ db/schema.sql
@@ -2,24 +2,27 @@ CREATE DATABASE IF NOT EXISTS lightspeed_db;
 
 USE lightspeed_db;
 
--- Create the table for Employees assigned to Projects
 CREATE TABLE employees
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- primary key column
-    first_name VARCHAR(50) NOT NULL
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
 );
 
 -- Create the table for Projects
 CREATE TABLE projects
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- primary key column
-    title VARCHAR(50) UNIQUE NOT NULL
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    title VARCHAR(50) UNIQUE NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
 );
 
 -- Create the table for Tasks on Each Project
 CREATE TABLE tasks
 (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- primary key column
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
     task_name VARCHAR(50) NOT NULL,
     duration INT NOT NULL,
     project_id INT UNSIGNED NOT NULL,
@@ -27,5 +30,7 @@ CREATE TABLE tasks
     CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES projects(id),
     employee_id INT UNSIGNED NOT NULL,
     INDEX employee_idx (employee_id),
-    CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employees(id)
+    CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employees(id),
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
 );
